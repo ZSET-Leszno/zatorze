7ccc6652e496bdae996f51c832d727ea3fa5c699
Stanislaw Gubanski <stasineky@protonmail.com>
2023-03-08 11:26:50 +0100

    er

--- panel_admin.php
+++ panel_admin.php
@@ -1,40 +1,41 @@
-// panel administraotra gdzie można dodawać posy
- <?php
-     // sprawdzenie czy użytkownik jest zalogowany
-     session_start();
+<?php
+session_start();
      if(!isset($_SESSION['admin'])){
-         header('Location: admin_login.php');
-     }
- ?>
- <!DOCTYPE html>
- <html lang="en">
- <head>
-        <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+               header('Location: admin_login.php');
+                 }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Panel administratora</title>
- </head>
- <body>
-     <h1>Panel administratora</h1>
+    <title>Panel administratora</title>
+</head>
+<body>
+    <h1>Panel administratora</h1>
+    <a href="logout.php">Wyloguj</a>
     <form action="panel_admin.php" method="post">
-         <input type="text" name="title" placeholder="Tytuł">
-                  <textarea name="content" cols="30" rows="10" placeholder="Treść"></textarea>
-         <input type="submit" value="Dodaj">
-     </form>
-    <?php
-         // sprawdzenie czy zostały przesłane dane
-         if(isset($_POST['title']) && isset($_POST['content'])){
-             // połączenie z bazą danych
-             require_once 'conn.php';
-             // dodanie posta do bazy danych
-             $sql = "INSERT INTO posts (title, content) VALUES ('{$_POST['title']}', '{$_POST['content']}')";
-             $result = mysqli_query($conn, $sql);
-             if($result){
-                 echo 'Dodano post';
-             } else {
-                 echo 'Błąd dodawania posta';
-             }
-         }
-     ?>
- </body>
- </html>
+        <input type="text" name="title" placeholder="Tytuł">
+        <textarea name="content" cols="30" rows="10" placeholder="Treść"></textarea>
+        <input type="file" name="image">
+        <input type="submit" value="Dodaj">
+      </form>
+      <?php
+        // sprawdzenie czy zostały przesłane dane
+        if(isset($_POST['title']) && isset($_POST['content'])){
+            // połączenie z bazą danych
+            require_once 'conn.php';
+            // dodanie wpisu do bazy danych
+            $sql = "INSERT INTO posts (title, content, image) VALUES ('{$_POST['title']}', '{$_POST['content']}', '{$_FILES['image']['name']}')";            $result = mysqli_query($conn, $sql);
+            // sprawdzenie czy dodanie wpisu się powiodło
+            if($result){
+                echo 'Dodano wpis';
+            } else {
+                echo 'Wystąpił błąd podczas dodawania wpisu';
+            }
+        }
+      ?>
+</body>
+</html>
+
